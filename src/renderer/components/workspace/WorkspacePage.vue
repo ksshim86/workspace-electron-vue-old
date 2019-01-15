c41d8ceb280032eaf477e262cc37e01f9f8b3d9c
심규승 <ksshim86@gmail.com>
2019-01-16 00:22:15 +0900

    flex layout 설정 성공!

--- src/renderer/components/workspace/WorkspacePage.vue
+++ src/renderer/components/workspace/WorkspacePage.vue
@@ -1,51 +1,48 @@
 <template>
-    <v-layout row fill-height style="background-color: red;">
-      <v-navigation-drawer permanent :mini-variant="navigation.miniVariant" >
-        <v-toolbar class="transparent" flat>
-          <v-list>
-            <v-list-tile>
-              <v-list-tile-title class="title">
-                Workspace
-              </v-list-tile-title>
+  <v-layout row fill-height style="background-color: red;">
+    <v-navigation-drawer permanent :mini-variant="navigation.miniVariant">
+      <v-toolbar class="transparent" flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Workspace
+            </v-list-tile-title>
+          </v-list-tile>
+          <v-divider></v-divider>
+          <v-list-group v-for="item in items" :key="item.title" v-model="item.active" :prepend-icon="item.action" no-action>
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
             </v-list-tile>
-            <v-divider></v-divider>
-            <v-list-group 
-              v-for="item in items"
-              :key="item.title"
-              v-model="item.active"
-              :prepend-icon="item.action"
-              no-action
-            >
-              <v-list-tile slot="activator">
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-              <v-list-tile
-                v-for="subItem in item.items"
-                :key="subItem.title"
-              >
-                <v-list-tile-content>
-                  <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
-                </v-list-tile-content>
-  
-                <v-list-tile-action>
-                  <v-icon>{{ subItem.action }}</v-icon>
-                </v-list-tile-action>
-              </v-list-tile>
-            </v-list-group>
+            <v-list-tile v-for="subItem in item.items" :key="subItem.title">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+              </v-list-tile-content>
 
-          </v-list>
-        </v-toolbar>
-      </v-navigation-drawer>
-        <v-layout row wrap>
-          <v-flex sm12 md12 style="background-color: orange;"></v-flex>
-          <v-flex sm12 md4 style="background-color: gray;"></v-flex>
-          <v-flex sm12 md4 style="background-color: white;"></v-flex>
-          <v-flex sm12 md4 style="background-color: gray;"></v-flex>
-        </v-layout>
-      <!-- </v-flex> -->
+              <v-list-tile-action>
+                <v-icon>{{ subItem.action }}</v-icon>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list-group>
+
+        </v-list>
+      </v-toolbar>
+    </v-navigation-drawer>
+    <v-layout column style="background-color: green;">
+      <v-toolbar></v-toolbar>
+      <v-layout row wrap style="backgroun-color: yellow;">
+        <v-flex sm12 md4 style="background-color: gray;"></v-flex>
+        <v-flex sm12 md4 style="background-color: white;"></v-flex>
+        <v-flex sm12 md4 style="background-color: gray;"></v-flex>
+      </v-layout>
     </v-layout>
+    <!-- 
+      <v-flex row sm12 md4 style="background-color: gray;"></v-flex>
+      <v-flex row sm12 md4 style="background-color: white;"></v-flex>
+      <v-flex row sm12 md4 style="background-color: gray;"></v-flex>
+     -->
+  </v-layout>
 </template>
 
 <script>
